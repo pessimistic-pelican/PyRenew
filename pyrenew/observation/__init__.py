658cc406ba785fa90bb65d8561d756498ae3154b
Jing Jie Ong <jingjieong12@gmail.com>
2025-09-25 18:40:43 +0800

    added Gamma obs process to init

--- pyrenew/observation/__init__.py
+++ pyrenew/observation/__init__.py
@@ -2,8 +2,10 @@
 
 from pyrenew.observation.negativebinomial import NegativeBinomialObservation
 from pyrenew.observation.poisson import PoissonObservation
+from pyrenew.observation.gamma import GammaObservation
 
 __all__ = [
     "NegativeBinomialObservation",
     "PoissonObservation",
+    "GammaObservation"
 ]
